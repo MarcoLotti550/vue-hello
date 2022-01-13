5fb9096508b8fd4f0f1ffd970c5d215c1024743c
MarcoLotti <gorlandelli64@gmail.com>
2022-01-13 13:13:51 +0100

    fine

---
+++ main.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+    el: '#newapp',
+    data: {
+        ciao: "hello world",
+        image: "https://www.laramind.com/blog/wp-content/uploads/2020/01/vue-620x420.png",
+    },
+    methods: {
+        
+    },
+});
